d83b0c0ed15d1c3b5b8548914054bfebdbfa3c25
mukesher4 <115150217+mukesher4@users.noreply.github.com>
2023-06-22 15:24:37 +0530

    Update Q10uncorrected.c

--- Q10uncorrected.c
+++ Q10uncorrected.c
@@ -11,7 +11,7 @@ void main(){
 	printf("Enter real part of complex no. 1 & 2");
 	scanf("%d%d",&cn.real[0],&cn.real[1]);
 	printf("Enter complex part of complex no. 1 & 2");
-	scanf("%d%d",&cn.complex[0],cn.complex[1]);
+	scanf("%d%d",&cn.complex[0],&cn.complex[1]);
 	cn.result[0]=cn.real[0]*cn.real[1]-cn.complex[0]*cn.complex[1];
 	cn.result[0]=cn.real[0]*cn.complex[1]+cn.complex[0]*cn.real[1];
 
